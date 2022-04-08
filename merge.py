015cf7c9404509658dabda1cea8b875702bb248c
Vidar <44647279+radivd@users.noreply.github.com>
2022-04-08 14:22:24 +0200

    code cleanup merge.py

--- merge.py
+++ merge.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import re
 
+minLength = 8
+maxLength = 25
 input1 = open("input1.txt", "r", encoding="utf8")
 input2 = open("input2.txt", "r", encoding="utf8")
 list1 = input1.readlines()
@@ -8,28 +10,28 @@ list2 = input2.readlines()
 list = []
 dict = {}
 
+
+def addListToDict(val):
+    for word in val:
+        word_length = len(word)
+        if minLength <= word_length <= maxLength:
+            dict[word] = True
+
+
 print("Adding words from file 1")
-for word in list1:
-    word_len = len(word)
-    if (word_len >= 8 and word_len <= 25):
-        dict[word] = True
+addListToDict(list1)
 
 print("Adding words from file 2")
-for word in list2:
-    word_len = len(word)
-    if (word_len >= 8 and word_len <= 25):
-        dict[word] = True
+addListToDict(list2)
 
 print("Copying words from Dict to List")
 for word in dict:
     list.append(word)
 
-print(len(list))
-
 print("Sorting")
 list.sort()
 
 print("Exporting words to file")
-with open("output3.txt", 'w', encoding="utf8") as file:
-    for row in list:
-        file.write(row)
+with open("merge-output.txt", 'w', encoding="utf8") as file:
+    for word in list:
+        file.write(word)
